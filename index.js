ddbec8a594edcf35f65a8cf7f0ad2258ebe35c1e
phpwechat <3257132998@qq.com>
2022-08-28 20:43:42 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 const ajaxPara2Apipost = require('ajax2apipost'),
-    _ = require("lodash"),
-    extTools = require('apipost-tools'),
-    JSON5 = require('json5'),
-    apipostRequest = require('apipost-send');
+  _ = require('lodash'),
+  extTools = require('apipost-tools'),
+  JSON5 = require('json5'),
+  apipostRequest = require('apipost-send');
 
 function nodeAjax(options) {
-    let _target = ajaxPara2Apipost(options);
-    let request = new apipostRequest({});
+  if (typeof arguments[1] === 'function' && _.isString(arguments[0])) {
+    options = {
+      url: arguments[0],
+      method: 'GET',
+      success: arguments[1],
+      data: {},
+    };
+  }
+  const _target = ajaxPara2Apipost(options);
 
-    request.request(_target).then((data) => {
-        if (_.isObject(data) && data.status == "success" && _.has(data, 'data.response.rawBody')) {
-            let res = data.data.response.rawBody;
-            if (extTools.isJson5(data.data.response.rawBody)) {
-                res = JSON5.parse(data.data.response.rawBody);
-            }
+  const request = new apipostRequest({});
 
-            if (_.has(options, 'success') && _.isFunction(options.success)) {
-                options.success(res, 'success', null);
-            }
+  request.request(_target).then((data) => {
+    if (_.isObject(data) && data.status == 'success' && _.has(data, 'data.response.rawBody')) {
+      let res = data.data.response.rawBody;
+      if (extTools.isJson5(data.data.response.rawBody)) {
+        res = JSON5.parse(data.data.response.rawBody);
+      }
 
-            if (_.has(options, 'complete') && _.isFunction(options.complete)) {
-                options.complete(null, 'success');
-            }
-        }
-    }).catch(err => {
-        if (_.has(options, 'error') && _.isFunction(options.error)) {
-            options.error(null, 'error', err);
-        }
+      if (_.has(options, 'success') && _.isFunction(options.success)) {
+        options.success(res, 'success', null);
+      }
 
-        if (_.has(options, 'complete') && _.isFunction(options.complete)) {
-            options.complete(null, 'success');
-        }
-    });
+      if (_.has(options, 'complete') && _.isFunction(options.complete)) {
+        options.complete(null, 'success');
+      }
+    }
+  }).catch((err) => {
+    if (_.has(options, 'error') && _.isFunction(options.error)) {
+      options.error(null, 'error', err);
+    }
+
+    if (_.has(options, 'complete') && _.isFunction(options.complete)) {
+      options.complete(null, 'success');
+    }
+  });
 }
 
 module.exports = nodeAjax;
